95abb75883721751588e5c05b151371bb0e14500
Alexandre Rocha <juny14_15@hotmail.com>
2026-03-14 13:00:43 -0300

    Adicionado Exercicio 05

---
+++ Exercicio05/Program.cs
@@ -0,0 +1,20 @@
+﻿// 5. Crie um programa para calcular o salário total de um vendedor. Deverá ser informado o salário base e o total de
+//    vendas. A comissão é calculada com um percentual (informado pelo usuário) sobre o total de vendas.
+
+using System.Diagnostics.Contracts;
+
+Console.Write("Informe o salário base: ");
+decimal salario = Convert.ToDecimal(Console.ReadLine());
+
+Console.Write("Informe o total de vendas: ");
+decimal vendas = Convert.ToDecimal(Console.ReadLine());
+
+Console.Write("Informe o percentual da comissão: ");
+decimal porcentagem = Convert.ToDecimal(Console.ReadLine());
+
+decimal comissao = vendas * porcentagem / 100;
+
+decimal salarioTotal = salario + comissao;
+
+Console.WriteLine();
+Console.WriteLine($"O salário total é de: {salarioTotal}");
