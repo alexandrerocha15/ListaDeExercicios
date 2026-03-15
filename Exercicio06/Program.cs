efd89284c9a0dec086888b1c96ae4ed2b6d91407
Alexandre Rocha <juny14_15@hotmail.com>
2026-03-15 00:03:10 -0300

    Adicionando Exercicio 06

---
+++ Exercicio06/Program.cs
@@ -0,0 +1,42 @@
+﻿// 6. Crie um programa para calcular a média harmônica das notas de um Aluno
+
+using System.Diagnostics.Contracts;
+
+Console.WriteLine("-------------------------");
+Console.WriteLine("Calcular nota Harmônica");
+Console.WriteLine("-------------------------");
+Console.WriteLine();
+
+Console.WriteLine(" - = - - = - - = - - = - - = - ");
+Console.Write("Informe a quantidade de notas: ");
+int qtdNotas = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine();
+
+
+decimal[] notas = new decimal[qtdNotas];
+int notasInseridas = 0;
+
+Console.WriteLine("Digite as notas do Aluno(a) ou 'sair' para encerrar");
+for(int contador = 0; contador < qtdNotas; contador++)
+{
+    Console.WriteLine("Digite o valor da nota ou 'sair' para encerrar: ");
+    Console.Write($"Nota '{contador + 1}': ");
+    string? notaParcial = Console.ReadLine();
+    Console.WriteLine();
+    if (notaParcial?.ToUpper() == "SAIR")
+        break;
+    decimal nota = Convert.ToDecimal(notaParcial);
+    notas[contador] = nota;
+    notasInseridas++;
+}
+
+decimal soma = 0;
+
+for (int i = 0; i < notasInseridas; i++)
+{
+    soma += 1 / notas[i];
+}
+
+decimal mediaHarmonica = notasInseridas / soma;
+
+Console.WriteLine($"A média harmônica: {mediaHarmonica:F2}");
