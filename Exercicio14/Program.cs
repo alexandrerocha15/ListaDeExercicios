d7148ae927f3ea5c39a98b3d4c8b496f4ffb80c2
Alexandre Rocha <juny14_15@hotmail.com>
2026-03-15 21:32:59 -0300

    Adicionando Exercicio 14

---
+++ Exercicio14/Program.cs
@@ -0,0 +1,27 @@
+﻿// 14. Escreva um algoritmo que leia três valores inteiros e diferentes e mostre-os em 
+// ordem decrescente.
+
+int[] numeros = new int[3];
+
+Console.Clear();
+Console.WriteLine("- = - = - = - = - = - = - = -");
+Console.WriteLine("Insira 3 valores inteiros");
+Console.Write("Valor 1: ");
+numeros[0] = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Valor 2: ");
+numeros[1] = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Valor 3: ");
+numeros[2] = Convert.ToInt32(Console.ReadLine());
+
+Array.Sort(numeros);
+Array.Reverse(numeros);
+
+Console.WriteLine();
+Console.WriteLine("- = - = - = - = - = - = - = -");
+Console.WriteLine("Novos Valores");
+for (int i = 0; i < 3; i++)
+{
+    Console.WriteLine($"Valor {i + 1}: {numeros[i]}");
+}
